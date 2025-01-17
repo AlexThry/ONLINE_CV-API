b4ded2fa8c4e0461c144ffa104e7a0aa944339bf
Alexis Thierry <alexist103@gmail.com>
2025-01-17 19:33:22 +0200

    [fix] change cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: '*',
+    origin: 'https://cv.alexis-thierry.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
