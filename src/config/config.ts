4a61b0c64e60878785b265977228f7810c3fd25f
Alexis Thierry <alexist103@gmail.com>
2025-02-06 21:29:54 +0200

    adding env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 export const config = () => ({
-    TOKEN_GITHUB: process.env.TOKEN_GITHUB || ""
+    TOKEN_GITHUB: process.env.TOKEN_GITHUB || "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"
 })
